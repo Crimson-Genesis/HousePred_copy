2ac521626bc10abca04a03773e4474d347e61ee9
Crimson-Genesis <nico.zero.0x@gmail.com>
2025-07-25 09:23:55 +0530

    changes in the project settings.py file for getting the security key through enviroment vairable

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
-import pandas as pd
+# import pandas as pd
+# data = pd.read_csv("./USA_Housing.csv");
+# y = data["Price"] * 84;
+#
+# print(y[1]);
+# print(data["Price"][1]);
 
-data = pd.read_csv("./USA_Housing.csv");
-y = data["Price"] * 84;
 
-print(y[1]);
-print(data["Price"][1]);
+import os
+
+sec_key = os.getenv("DJANGO_SECRET_KEY");
+
+print(sec_key);
